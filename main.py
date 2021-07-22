7ac8466dd66db16bc011a468676402c965532ac5
Phil <phillip.huval@gmail.com>
2021-07-22 00:07:22 -0500

    added hello world bot and added async to main

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import asyncio
 import time
 
 options = Options()
-# options.headless = True
-driver = webdriver.Chrome(options=options)
-driver.get("http://raidbots.com/simbot/quick?region=us&realm=malganis&name=garctic")
-print (driver.current_url)
-driver.save_screenshot("screenshot.png")
-time.sleep(5)
-sim_text = driver.find_elements_by_xpath("//*[contains(text(), 'Run Quick Sim')]")
-print(sim_text[0])
-flex = sim_text[0].find_element_by_xpath('..')
-sim_button = flex.find_element_by_xpath('..')
-sim_button.click()
-time.sleep(5)
+options.headless = True
 
-# driver.find_element_by_css_selector('.Button').click()
-print (driver.current_url)
-# driver.close()
+async def runSim(char_name):
+	driver = webdriver.Chrome('bin/chromedriver', options=options)
+
+
+	driver.get('http://raidbots.com/simbot/quick' + 
+		f'?region=us&realm=malganis&name={char_name}')
+	print (driver.current_url)
+	await asyncio.sleep(5)
+	sim_text = driver.find_elements_by_xpath("//*[contains(text(), 'Run Quick Sim')]")
+	flex = sim_text[0].find_element_by_xpath('..')
+	sim_button = flex.find_element_by_xpath('..')
+	sim_button.click()
+	await asyncio.sleep(5)
+
+	report_url = driver.current_url
+	driver.close()
+	return char_name, report_url
+
+async def main():
+	# loop = asyncio.get_event_loop()
+	lst = [
+		asyncio.create_task(runSim('garctic')), 
+		asyncio.create_task(runSim('joltheus'))
+	]
+	results = await asyncio.gather(*lst)
+	print(results)
+
+asyncio.run(main())
